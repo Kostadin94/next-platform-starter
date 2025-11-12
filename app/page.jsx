289bb888e9d65ca07bbc12d035e84aae7c573d7a
kosta <kosta.rajkovic@gmail.com>
2025-11-13 00:31:17 +0100

    Obrisi text

--- app/page.jsx
+++ app/page.jsx
@@ -5,15 +5,6 @@ import { Markdown } from 'components/markdown';
 import { RandomQuote } from 'components/random-quote';
 import { getNetlifyContext } from 'utils';
 
-const contextExplainer = `
-The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
-([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
-`;
-
-const preDynamicContentExplainer = `
-The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
-`;
-
 const ctx = getNetlifyContext();
 
 export default function Page() {
@@ -31,17 +22,15 @@ export default function Page() {
                 <Link href="/">
                 <Image src='images/image1.jpg' alt="Netlify logo" />
                 </Link>
-                <Image src='public/images/image2.jpg' alt="Netlify logo" />
-                <Image src='public/images/image3.jpg' alt="Netlify logo" />
+                <Image src='images/image2.jpg' alt="Netlify logo" />
+                <Image src='images/image3.jpg' alt="Netlify logo" />
             </section>
             {!!ctx && (
                 <section className="flex flex-col gap-4">
-                    <Markdown content={contextExplainer} />
                     <RuntimeContextCard />
                 </section>
             )}
             <section className="flex flex-col gap-4">
-                <Markdown content={preDynamicContentExplainer} />
                 <RandomQuote />
             </section>
         </div>
